fb9f4569fc58d8341952ff3019d10898e6e98f04
David Nuon <david@davidnuon.com>
2012-06-01 10:15:03 -0700

    TODO: Finish the q.a.d render

--- production/acm-meeting.py
+++ production/acm-meeting.py
@@ -1,1 +1,23 @@
-#!/usr/bin/env python 
+"""
+ACM Meeting Script
+
+Author: Shane Satterfield
+Author: David Nuon
+"""
+
+#!/usr/bin/env python
+import sys
+sys.path.insert(0, './app/lib')
+sys.path.insert(0, './app/')
+
+import argparse
+import am_config
+import argparse
+
+parser = argparse.ArgumentParser()
+
+## Command List
+parser.add_argument('--render', '-rn', help="Prints the HTML document from markdwn." )
+parser_args = parser.parse_args()
+
+print parser_args
--- production/app/am_config.py
+++ production/app/am_config.py
@@ -4,3 +4,7 @@ FTP_HOST = ''
 FTP_USER = ''
 FTP_PASS = ''
 
+# Template values
+# CSS
+SCREEN_CSS = ''
+PRINT_CSS  = ''
