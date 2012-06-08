58d14d5c2097e1e2e67f0834dbd8aaf91becef90
David Nuon <david@davidnuon.com>
2012-06-08 11:55:00 -0700

    Removed test from Agenda Class

--- production/app/model/agenda.py
+++ production/app/model/agenda.py
@@ -31,22 +31,3 @@ class Agenda(MarkdownList):
 		self.append(business)
 
 		self.append("Adjournment")
-
-if __name__ == "__main__":
-	testdata = """name,position,email,exec
-	David Nuon,President,chair@csulb.acm.org,1
-	Ian Goegebuer,Vice President,vicechair@csulb.acm.org,1
-	Christopher Gomez,Treasurer,treasurer@csulb.acm.org,1
-	Shane Satterfield,Secretary,secretary@csulb.acm.org,1
-	Victoria Hatfield,AESB Representative,aesbrep@csulb.acm.org,
-	Anthony Gialcalone,Librarian,librarian@csulb.acm.org,
-	Leo Tronolone,Publicity and Recruitment Chair,publicity@csulb.acm.org,
-	James Coolidge,Market Manager,vendor@csulb.acm.org,
-	Orion Sakorn,Historian,historian@csulb.acm.org,
-	Diana Ignacio,Event Coordinator,events@csulb.acm.org,
-	John-Jimi Kathleen Som,Webmaster,webmaster@csulb.acm.org,
-	Johnny Patterson,Office Technician,tech@csulb.acm.org,
-	Graham Otte,Codename: Fattybanana,tech@csulb.acm.org,2"""
-
-	a = Agenda(testdata)
-	print a.markdown()
